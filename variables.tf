9d842ede894298f23bde28da9a01911f9bd05424
Jamie Taffurelli <jltaffurelli@outlook.com>
2023-01-13 17:17:12 +0000

    initial module upload

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the Resource Group to deploy to"
+}
+
+variable "location" {
+  type        = string
+  description = "Location to deploy resources"
+}
+
+variable "user_assigned_identity_name" {
+  type        = string
+  description = "Name of user assigned identity for image builder"
+}
+
+variable "gallery_name" {
+  type        = string
+  description = "Name of Shared Image Gallery"
+}
+
+variable "windows_image_templates" {
+  type = list(object({
+    name                 = string
+    image_name           = string
+    artifact_tags        = list(string)
+    hardening_script_url = string
+  }))
+  description = "Windows image templates to deploy"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags to apply to resources"
+}
